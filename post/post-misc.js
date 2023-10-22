03b387fd6c10e04525c36c97b56a5804469b5b16
qngg <124476435+qngg@users.noreply.github.com>
2023-10-22 12:45:59 +0000

    quick fix for post subpage

---
+++ post/post-misc.js
@@ -0,0 +1,7 @@
+function post()
+{
+    const id = Math.random(1,100000000000000).toString();
+    const name = document.getElementById("name").value;
+    const content = document.getElementById("content").value;
+    
+}
