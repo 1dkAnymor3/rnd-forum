7349e423d93604de362c2bfc2759de9bcd7bb392
qngg <124476435+qngg@users.noreply.github.com>
2023-10-24 10:51:16 +0000

    idk what to name this

--- post/post-misc.js
+++ post/post-misc.js
@@ -1,7 +1,34 @@
-function post()
+import allPost from './post.json' assert { type: 'json' };
+import { Octokit, App } from "https://esm.sh/octokit";
+
+function gup(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return "";
+    else
+        return results[1];
+}
+
+async function post()
 {
-    const id = Math.random(1,100000000000000).toString();
+    const octokit = new Octokit({ });
+    let id = "0";
+    while (allPost[id] != null) id = Math.random(1,100000000000000).toString();
     const name = document.getElementById("name").value;
     const content = document.getElementById("content").value;
-    
+}
+
+function loadPost()
+{
+    const id = gup("id");
+    const titleElement = document.getElementById("postTitle");
+    const contentElement = document.getElementById("postContent");
+    const dateElement = document.getElementById("postDate");
+    const postData = allPost[id.toString()];
+    titleElement.innerHTML = postData["name"];
+    contentElement.innerHTML = postData["content"];
+    dateElement.innerHTML = postData["date"];
 }
